580f1889839ef8f4f1525488e10dda0f56206284
Madan.patakota <madan.patakota@gmail.com>
2026-09-08 09:39:48 +0530

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component , signal , computed  , effect} from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('step4_ComponentsCommunication');
+
+  /* signal is reactive varaible in angular which stores a value and inform angular 
+      when that value changes */
+
+
+     employeeName   = signal('Peter');
+     basicSalary    = signal(30000);
+
+
+     constructor(){
+
+       //its a function is automatically run as a side-effect wenever
+       // the signals read inside that function change..
+         effect(()=>{
+              console.log(`${this.employeeName()} | Salary: ₹ ${this.basicSalary()}`);
+         })
+     }
+
+
+     /* its a read-only signal whose value is automatically 
+         calculated from one or more other signals*/
+     yearlySalary = computed(()=> this.basicSalary() * 12 );
+
+
+     changeEmployeeName(){
+       this.employeeName.set("Rebert.");
+     }
+
+     setSalary(){
+      this.basicSalary.set(50000);
+     }
+
+     updateSalary(){
+      this.basicSalary.update((currentSalary)=> currentSalary + 5000);
+     }
+
+
 }
